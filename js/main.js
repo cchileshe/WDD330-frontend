7b32c0c5a89e55772ef67e6a5f0cb7d46a798305
cchileshe <chi06001@byui.edu>
2021-09-24 12:15:26 +0200

    week02 complete

--- js/main.js
+++ js/main.js
@@ -4,3 +4,22 @@ const links = [
       url: "week1/index.html"
     }
   ]
+
+     
+const toggleMenu = () => {
+  document.querySelector('#navigation').classList.toggle('display');
+}
+
+document.querySelector('#navigation').addEventListener('click', toggleMenu);
+
+function sendAlert() {
+  alert("This is an alert() method!");
+}
+
+function scrollBottom() {
+  window.scrollTo(0,5000);
+}
+
+function scrollBack() {
+  window.scrollTo(0,50);
+}
