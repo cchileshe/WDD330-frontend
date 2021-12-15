4aab337e5d5e35fa3dfc0ae5a56abc134745d8c7
cchileshe <chi06001@byui.edu>
2021-12-15 10:14:53 +0100

    progress workouts list done

--- final/assets/js/main.js
+++ final/assets/js/main.js
@@ -1,10 +1,16 @@
+
+import * as workouts from "../../model/data.js"; 
+// import Chart from '/chart.js/auto';
+
+// Chart.register(...registerables);
+
             
 //=============================
 // CURRENT DATE
 //=============================
 const Event = new Date();
 const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
-document.getElementById("dateHeading").innerHTML=Event.toLocaleDateString(undefined, options);
+document.getElementById("dateHeading").innerHTML= Event.toLocaleDateString(undefined, options);
 
 
 //=============================
@@ -34,9 +40,82 @@ function openNav() {
   });
 
 //=============================
-// LOCAL STORAGE WORKOUTS
+// PROGRESS CHARTS
 //=============================
-//console.log(workouts.weeks);
+// var require= chart;
+// const Chart = require('chart.js');
+// const myChart = new Chart(ctx, {...});
 
 
-// localStorage.setItem();
+
+$(document).ready(function() {
+  
+  let weekCount = 1;
+ 
+  workouts.weeks.slice(0, 1).forEach(week => {
+  
+    week.slice(0, 3).forEach(day => {
+      let selected = workouts.workoutList.filter(workout => workout.id == day.workout_id);
+      $(' <div class="row upcoming-item"><div class="col-1"><div class="checker"><input class="form-check-input" type="checkbox"></div></div><!--end checkbox --> <div class="col-6"> <span class="pt-1 form-checked-content"><strong>' + selected[0].name +'</strong><small class="d-block text-muted"><i class="pull-right bi-calendar-date"></i><span> '+ day.day + '</span></small></span> <a href="javascript:void(0);" class="float-right remove-todo-item"><i class="icon-close"></i></a></div><!--end middle column --><div class="col"><a href="" class="nav-link right">Details <i class="bi bi-info-circle"></i></a></div></div>').appendTo("#upcoming");
+    });
+    weekCount;
+  });
+  
+ });
+
+const weeklyProgress = document.getElementById('weekly');
+const monthlyProgress = document.getElementById('monthly');
+const overallProgress = document.getElementById('overall');
+
+const weeklyChart = new Chart(weeklyProgress, {
+      type: 'doughnut',
+      data: {
+        datasets: [{
+          label: '20%',
+          data: [12, 19,],
+          backgroundColor: [
+              'rgba(248,195,70)',
+              'rgba(237,237,239)',
+              
+          ],
+          cutout:60
+      }]
+    }
+    // workouts.weeks.filter(workout => workout.id == day.workout_id)}
+});
+
+
+const monthlyChart = new Chart(monthlyProgress, {
+  type: 'doughnut',
+  data: {
+    datasets: [{
+      label: '20%',
+      data: [12, 19,],
+      backgroundColor: [
+          'rgba(90,216,249)',
+          'rgba(237,237,239)',
+          
+      ],
+      cutout:60
+  }]
+}
+// workouts.weeks.filter(workout => workout.id == day.workout_id)}
+});
+
+
+const overallChart = new Chart(overallProgress, {
+  type: 'doughnut',
+  data: {
+    datasets: [{
+      label: '20%',
+      data: [12, 19,],
+      backgroundColor: [
+          'rgba(95,204,54)',
+          'rgba(237,237,239)',
+          
+      ],
+      cutout:62
+  }]
+}
+// workouts.weeks.filter(workout => workout.id == day.workout_id)}
+});
--- final/assets/js/timer.js
+++ final/assets/js/timer.js
@@ -8,7 +8,9 @@ class Timer {
       minutes: root.querySelector(".timer__part--minutes"),
       seconds: root.querySelector(".timer__part--seconds"),
       control: document.querySelector(".timer__btn--control"),
-      reset: root.querySelector(".timer__btn--reset")
+      start: root.querySelector("#start"),
+      reset: root.querySelector("#reset"),
+      pause: root.querySelector("#pause")
     };
     this.day = new Date();
     this.options = {
@@ -26,26 +28,28 @@ class Timer {
      this.tempHistory = []
     this.remainingSeconds = localStorage.getItem('previousTime') ? localStorage.getItem('previousTime') : 30 * 60;
     this.updateInterfaceTime();
-    this.count= 0
-    this.el.control.addEventListener("click", () => {
-      if (this.interval === null) {
-        this.start();
-      } else {
-        this.stop();
-        this.count++
+    this.el.start.addEventListener("click", () => {
+      this.interval === null
+      this.start();
+    });
 
+    this.el.reset.addEventListener("click", () => {
+      this.reset();
+    });
+
+    this.el.pause.addEventListener("click", () => {
+        this.stop();
         localStorage.setItem("previousTime", this.remainingSeconds);
         this.history.push({
           'time': this.remainingSeconds,
           'date': this.day.toLocaleDateString(undefined, this.options),
           'name': workouts.workoutList.filter(workout => workout.id == this.currentDay).name
-        });
+        })
         
         localStorage.setItem('history', JSON.stringify(this.history));
         this.getRecords()
-      }
-    });
-  }
+      });
+    }
 
   updateInterfaceTime() {
     const minutes = Math.floor(this.remainingSeconds / 60);
@@ -77,19 +81,30 @@ class Timer {
     this.interval = null;
   }
 
+  reset() {
+    
+  
+  }
+
   static getHTML() {
     return `
               <span class="mins timer__part timer__part--minutes">00</span>
               <span class="timer__part">mins</span>
               <span class="secs timer__part timer__part--seconds">00</span>
               <span class="timer__part">secs</span>
-              <div class="row"> 
-                <button type="button" class="btn btn-outline-info btn-circle btn-md">
+              <div id="button-row" class="d-grid gap-2 d-md-flex justify-content-md-center">
+              
+              <button id="start" type="button" class="btn btn-outline-secondary btn-circle btn-md">
                   <span class="material-icons">start</span>
                 </button>
-                <button type="button" class="btn btn-outline-secondary btn-circle btn-md">
+                <button id="pause" type="button" class="btn btn-outline-info btn-circle btn-md">
                   <span class="material-icons">pause</span>
                 </button>
+                <button id="reset" type="button" class="btn btn-outline-warning btn-circle btn-md">
+                  <span class="material-icons">reset</span>
+                </button>
+              </span>
+                
               </div>`
 
   }
@@ -113,74 +128,3 @@ new Timer(
   document.querySelector(".timer")
 );
 
-
-
-
-/*
-
-const el = document.querySelector(".clock");
-const bell = document.querySelector("audio");
-
-const mindiv = document.querySelector(".mins");
-const secdiv = document.querySelector(".secs");
-
-
-localStorage.setItem("btn", "focus");
-
-let initial, totalsecs, perc, paused, mins, seconds;
-
-// startBtn
-document.querySelector('#maxOut').addEventListener("click", () => {
-  let btn = localStorage.getItem("btn");
-  
-
-  if (btn === "focus") {
-    mins = +localStorage.getItem("focusTime") || 30; //set timer to 30
-  } else {
-    mins = +localStorage.getItem("breakTime") || 1; 
-  }
-
-//   second = 1000,
-// minute = second * 60,
-        
-  seconds = 1000;
-  totalsecs = mins * 60;
-  // setTimeout(decremenT(), 60);
-  // startBtn.style.transform = "scale(0)";
-  paused = true;
-});
-
-function decremenT() {
-  mindiv.textContent = Math.floor(seconds / 60);
-  secdiv.textContent = seconds % 60 > 9 ? seconds % 60 : `0${seconds % 60}`;
-  // if (circle.classList.contains("danger")) {
-  //   circle.classList.remove("danger");
-  // }
-
-  if (seconds > 0) {
-    perc = Math.ceil(((totalsecs - seconds) / totalsecs) * 100);
-    // setProgress(perc);
-    seconds--;
-    initial = window.setTimeout("decremenT()", 1000);
-    // if (seconds < 10) {
-    //   circle.classList.add("danger");
-    // }
-  } else {
-    mins = 0;
-    seconds = 0;
-    bell.play();
-    let btn = localStorage.getItem("btn");
-
-    if (btn === "focus") {
-      // startBtn.textContent = "start break";
-      // startBtn.classList.add("break");
-      localStorage.setItem("btn", "break");
-    } else {
-      // startBtn.classList.remove("break");
-      // startBtn.textContent = "start focus";
-      localStorage.setItem("btn", "focus");
-    }
-    startBtn.style.transform = "scale(1)";
-  }
-}
-*/
